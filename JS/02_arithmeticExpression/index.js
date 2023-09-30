68a7125eb4504921fe8db6b5b6f6fc73a2c97f6f
quanganh125 <quanganhk62@gmail.com>
2023-09-30 11:17:21 +0900

    Arithmetic expression

---
+++ JS/02_arithmeticExpression/index.js
@@ -0,0 +1,34 @@
+/*
+  Arithmetic expression is a combination of...
+  operands: value, variable, ...ect
+  operators: + - * / %,...
+  that can be valueated to a value
+*/
+
+/*
+  operator precedence
+  1. parenthesis ()
+  2. exponents
+  3. multiplication, devision
+  4. addition, abstraction
+*/
+
+let student = 21;
+
+// student = student + 1;
+// student = student - 1;
+// student = student * 2;
+// student = student / 2;
+
+// let extraStudent = student % 3;
+
+// student += 1;
+// student -= 1;
+// student *= 2;
+student /= 2;
+
+console.log(student);
+
+let result = 1 + 2 * (3+4);
+
+console.log(result);
