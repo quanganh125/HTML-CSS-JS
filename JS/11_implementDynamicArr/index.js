7d08202ab98ae50ead422fd19cd54fb05b9b19e2
quanganh125 <quanganhk62@gmail.com>
2023-10-02 21:27:04 +0900

    Initialize dynamic array

---
+++ JS/11_implementDynamicArr/index.js
@@ -0,0 +1,16 @@
+class DynamicArray {
+  #capacity = 0;
+  #size = 0;
+
+  constructor(capacity, size) {
+    this.#capacity = capacity;
+    this.#size = size;
+  }
+
+  getCapacity () {
+    return this.#capacity;
+  }
+}
+
+var test = new DynamicArray(3, 4);
+console.log(test.getCapacity);
