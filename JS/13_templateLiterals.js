0b6dd2468b9738ee23d5a3333de19dffbe6273f1
quanganh125 <quanganhk62@gmail.com>
2023-10-11 22:55:07 +0900

    template literal in ES6

---
+++ JS/13_templateLiterals.js
@@ -0,0 +1,12 @@
+// ES6
+//  template Literal allows you not have to concat string but write it inside ${}
+
+const age = 22;
+
+console.log(`Nam nay toi ${age} tuoi`)
+
+// ES5
+
+var firstName = 'David';
+
+var fullName = "Toi ten la: " + firstName;
